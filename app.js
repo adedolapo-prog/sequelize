b5efe47e852c050b6d3f94556c1434aa9769d53d
adedolapo <shayne.doly94@gmail.com>
2021-07-30 20:15:03 +0100

    done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 
-const { sequelize, User } = require("./models")
+const { sequelize, User, Post } = require("./models")
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
@@ -11,6 +11,49 @@ app.post("/users", async (req, res) => {
   try {
     const user = await User.create({ name, email, role })
     return res.status(200).json({ success: true, data: user })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ success: false, data: err })
+  }
+})
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.findAll()
+    return res.status(200).json({ success: true, data: users })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ success: false, data: err })
+  }
+})
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { uuid: req.params.id }, include: 'posts' })
+    return res.status(200).json({ success: true, data: user })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ success: false, data: err })
+  }
+})
+
+app.post("/posts", async (req, res) => {
+  try {
+    const { userUuid, body } = req.body
+    const user = await User.findOne({ where: { uuid: userUuid } })
+
+    const post = await Post.create({ body, userId: user.id })
+    return res.status(200).json({ success: true, data: post })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ success: false, data: err })
+  }
+})
+
+app.get("/posts", async (req, res) => {
+  try {
+    const posts = await Post.findAll({ include: ['user']})
+    return res.status(200).json({ success: true, data: posts })
   } catch (err) {
     console.log(err)
     return res.status(500).json({ success: false, data: err })
--- models/post.js
+++ models/post.js
@@ -1,45 +1,37 @@
 "use strict"
 const { Model } = require("sequelize")
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Post extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Post }) {
+    static associate({ User }) {
       // define association here
-      this.hasMany(Post, { foreignKey: "userId", as: "posts" })
+      this.belongsTo(User, { foreignKey: "userId", as: "user" })
     }
 
     toJSON() {
-      return { ...this.get(), id: undefined }
+      return { ...this.get(), id: undefined, userId: undefined }
     }
   }
-  User.init(
+  Post.init(
     {
       uuid: {
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4,
       },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      role: {
+      body: {
         type: DataTypes.STRING,
         allowNull: false,
       },
     },
     {
       sequelize,
-      tableName: "users",
-      modelName: "User",
+      tableName: "posts",
+      modelName: "Post",
     }
   )
-  return User
+  return Post
 }
